ce96c46401f67c6168d60c727cd1bbdc915563cd
ShadowsGtt <1025814447@qq.com>
2018-02-09 07:07:27 +0200

    update

--- server/include/database.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DATABASE_H
-#define _DATABASE_H
-#include "wechat.h"
-extern int exc_sql(MYSQL *,const char *) ;   /* 执行sql语句 */
-extern void connect_mysql(MYSQL *);
- int select_result(MYSQL *,const char *);
-
-#endif
--- server/source/main.c
+++
@@ -1,6 +0,0 @@
-#include "../include/wechat.h"
-int main()
-{
-    main_thread_func();
-    return 0;
-}
