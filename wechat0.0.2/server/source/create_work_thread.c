98a6f9308d8f5c7a83995d59a7bfcd1a10309057
ShadowsGtt <1025814447@qq.com>
2018-06-24 21:40:17 +0800

    update

--- wechat0.0.2/server/source/create_work_thread.c
+++ wechat0.0.2/server/source/create_work_thread.c
@@ -2,29 +2,24 @@
 #include "../include/database.h"
 
 /* @count:the number of working thread */
-thread_t * create_work_thread(int count)
+void create_work_thread(int count)
 {
     if(count <= 0 )
-        return NULL;
-    thread_t *p = (thread_t *)malloc(sizeof(thread_t)*count);
-    if(NULL == p)
-        return NULL;
+        return ;
 
     for(int i = 0;i < count;i++)
     {
         
-        if(pthread_create(&(p[i]->tid), NULL,thread_run, NULL) != 0) 
+        if(pthread_create(&(thread[i].tid), NULL,thread_run,NULL) != 0) 
         {
             perror("thread_create");
-            free(p);
-            return NULL;
+            return ;
         }
-        if(pipe(p[i].pipe) != 0)
+        if(pipe(thread[i].pipe) != 0)
         {
             perror("pipe create");
-            return NULL;
+            return ;
         }
 
     }
-    return p;
 }
--- wechat0.0.2/server/source/register_epoll_fd.c
+++ wechat0.0.2/server/source/register_epoll_fd.c
@@ -3,15 +3,16 @@
 /* 注册epoll事件 */
 void register_epoll_fd(int epollfd,int fd,int oneshot)
 {
+    //int epollfd = *epoll_fd;
     struct epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN | EPOLLET;
+    event.events = EPOLLIN | EPOLLET | EPOLLERR;
     if(oneshot)
     {
         event.events |= EPOLLONESHOT;
     }
     if(epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&event) == -1) 
     {
-        fprintf(stderr,"%d register failed!",fd);
+        fprintf(stderr,"fd:%d register failed!",fd);
     }
 }
